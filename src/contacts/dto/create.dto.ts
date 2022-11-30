c530cd56f7be65fcd8444e30cb764f3cf15bd771
gabrielkf <gabrielkf@gmail.com>
2022-11-29 21:23:49 -0300

    Add validation

--- src/contacts/dto/create.dto.ts
+++ src/contacts/dto/create.dto.ts
@@ -1,9 +1,24 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsEmail,
+  IsPhoneNumber,
+} from 'class-validator';
+
 export class CreateContactDto {
+  @IsNotEmpty()
   name: string;
 
+  @IsOptional()
+  @IsNumber()
   phone: string;
 
+  @IsOptional()
+  @IsEmail()
   email: string;
 
+  @IsOptional()
+  @IsPhoneNumber()
   whatsapp: string;
 }
